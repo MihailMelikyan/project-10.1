b4b276e066c85387f857341341c149cf32afe1b0
MihailMelikyan <vip.mizhgan62@gmail.com>
2024-09-22 12:18:03 +0300

    завершенное задание

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ from threading import Thread
 from datetime import datetime
 from time import sleep
 from pprint import pprint
-time_start_1 = datetime.now()
+
 def write_words(word_count,file_name):
     with open(file_name,'w',encoding = 'utf-8') as file:
         for i in range(word_count):
             file.write(f'Какое-то слово № {i + 1}\n')
             time.sleep(0.1)
     print(f'Завершилась запись в файл {file_name}')
-
+time_start = datetime.now()
 
 write_words(10, 'example1.txt')
 write_words(30, 'example2.txt')
 write_words(200, 'example3.txt')
 write_words(100, 'example4.txt')
-time_end_1 = datetime.now()
-print(f'Работа потоков длилась {time_end_1}')
-time_start_2 = datetime.now()
+time_end = datetime.now()
+print(f'Работа потоков длилась {time_end - time_start}')
+time_start = datetime.now()
 thr_first = Thread(target=write_words,args=(10, 'example5.txt'))
 thr_second = Thread(target=write_words,args=(30, 'example6.txt'))
 thr_third = Thread(target=write_words,args=(200, 'example7.txt'))
@@ -33,7 +33,6 @@ thr_first.join()
 thr_second.join()
 thr_third.join()
 thr_four.join()
+time_end = datetime.now()
 
-time_end_2 = datetime.now()
-
-print((f'Работа потоков длилась {time_end_2}'))
+print((f'Работа потоков длилась {time_end - time_start}'))
